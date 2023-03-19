71109255d1cc859b67c134eb9ebc392a9b582607
Rummansarker <rummansarker567@gmail.com>
2023-03-20 01:04:11 +0600

    added rest countries api

---
+++ js/countries.js
@@ -0,0 +1,24 @@
+const loadCountries = () => {
+  fetch('https://restcountries.com/v3.1/all')
+  .then(res => res.json())
+  .then(data => displayCountries(data))
+}
+
+const displayCountries = countries => {
+  // console.log(countries)
+  const countriesHTML = countries.map(country => showAllCountries(country))
+  // console.log(countriesHTML)
+  const container = document.getElementById('countries');
+  container.innerHTML = countriesHTML.join(' ');
+}
+
+const showAllCountries = country => {
+  return `
+    <div>
+      <h3>${country.name.common}</h3>
+      <img src = ${country.flags.png}>
+    </div>
+  `
+}
+
+loadCountries();
